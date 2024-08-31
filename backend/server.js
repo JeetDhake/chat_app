7dd595ab916ed9793d92e14610ea71e9b8bdb975
jeetdhake <dhakejeet@gmail.com>
2024-08-31 14:00:14 +0530

    server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv").config();
+
+app.get('/api/chat/', (req, res) => {
+    res.send("api running");
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    const singlechat = chats.find((c) => c._id === req.params.id);
+    res.send(singlechat);
+});
+
+const port = process.env.PORT || 7000;
+app.listen(port, console.log(`SERVER RUNNING: http://localhost:${port}/`));
