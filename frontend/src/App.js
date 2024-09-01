0ca0c1ea04f92c06a14307076e6498978e8b0a21
jeetdhake <dhakejeet@gmail.com>
2024-09-01 11:30:33 +0530

    component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './styles/App.css';
 import { Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/ChatPage';
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/LoginSignUp.css'
+import LoginPage from '../components/LoginPage';
+import SignupPage from '../components/SignupPage';
 
 const HomePage = () => {
   return (
     <div>
-      home
+      <LoginPage/>
+      <SignupPage/>
     </div>
   )
 }
